bd1df7b149acf23a2b5b5a5da2f2618f2b69280e
Administrator <Administrator@Si7A-34>
2020-07-29 09:44:40 +0900

    holly

---
+++ src/test/BatchTest.java
@@ -0,0 +1,37 @@
+package test;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import com.erp.test.common.Conn;
+
+public class BatchTest {
+
+	public static void main(String[] args) {
+		Connection con = Conn.open();
+		long sTime = System.currentTimeMillis();
+		String sql = "insert into batch_test values(?,?)";
+
+		try {
+			PreparedStatement ps = con.prepareStatement(sql);
+			for (int i = 1; i <= 1000000; i++) {
+				ps.setInt(1, i);
+				ps.setString(2, "이름" + i);
+				ps.addBatch();
+				if (i % 10000 == 0) {
+					System.out.println(i / 10000);
+					ps.executeBatch();
+					con.commit();
+					ps.clearBatch();
+				}
+			}
+			con.commit();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		long eTime = System.currentTimeMillis();
+		System.out.println("실행 시간: " + (eTime - sTime));
+	}
+}
